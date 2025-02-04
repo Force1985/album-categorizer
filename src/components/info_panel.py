a866e906330559aa0f989975febc4c8df053e4aa
Force <polgar.peter.force@gmail.com>
2025-02-04 23:13:16 +0100

    info sample textarea

--- src/components/info_panel.py
+++ src/components/info_panel.py
@@ -6,5 +6,26 @@ def render_info_panel():
     """
     with st.container():
         st.subheader('Album Information')
-        # Placeholder for future implementation
-        st.info('Information panel will be implemented here')
+        
+        template = """[artists_sort] - [title]
+
+Label: [labels.name]
+Catalog#: [labels.catno]
+Format: [formats.qty]x[formats.name], [formats.descriptions]
+Country: [country]
+Released: [released]
+Style: [styles]
+Notes: Written & produced by ???.
+[notes]
+Discogs: [uri]
+
+Tracklist:
+[tracklist.position]. [artists_sort] - [tracklist.title]    [tracklist.duration]
+    [extraartists.role] - [extraartists.name]"""
+
+        st.text_area(
+            label="Info Template",
+            value=template,
+            height=400,
+            disabled=True
+        )
